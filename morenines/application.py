ea8e9f77c894d09694d63cc99dbd0e7465d8ba45
Dan McGinnis <dan@mcgid.ca>
2016-03-22 17:38:37 -0400

    Add common params via decorator
    This commit is net +10 lines, but it centralizes the definition of
    common parameters, which may lead to fewer typos, and definitely feels
    more designed. The ceremony required for decorators with arguments isn't
    too onerous.

--- morenines/application.py
+++ morenines/application.py
@@ -15,21 +15,33 @@ import morenines.output
 #    resolve_path=True,
 #)
 
+_common_params = {
+    'index': click.option('--index', 'index_path', required=True),
+    'root_path': click.argument('root_path', nargs=1, default=os.getcwd()),
+}
+
+def common_params(*param_names):
+    def real_decorator(func):
+        for param_name in param_names:
+            func = _common_params[param_name](func)
+        return func
+
+    return real_decorator
+
 @click.group()
 def main():
     pass
 
 
 @main.command()
-@click.argument('root_path', nargs=1, default=os.getcwd())
+@common_params('root_path')
 def create(root_path):
     index = morenines.create.create(root_path)
 
     index.write(sys.stdout)
 
 @main.command()
-@click.option('--index', 'index_path', required=True)
-@click.argument('root_path', nargs=1, default=os.getcwd())
+@common_params('index', 'root_path')
 def verify(root_path, index_path):
     changed_files, missing_files = morenines.verify.verify(root_path, index_path)
 
@@ -37,16 +49,14 @@ def verify(root_path, index_path):
 
 
 @main.command()
-@click.option('--index', 'index_path', required=True)
-@click.argument('root_path', nargs=1, default=os.getcwd())
+@common_params('index', 'root_path')
 def status(root_path, index_path):
     new_files, missing_files = morenines.status.status(root_path, index_path)
     morenines.output.print_filelists(new_files, None, missing_files)
 
 
 @main.command()
-@click.option('--index', 'index_path', required=True)
-@click.argument('root_path', nargs=1, default=os.getcwd())
+@common_params('index', 'root_path')
 def push(root_path, index_path):
     remotes = [morenines.remote.FakeRemote(None)]
     morenines.push.push(root_path, index_path, remotes)
