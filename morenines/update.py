54fec332ef70f2b3c0eada9453f62a6b3f93852f
Dan McGinnis <dan@mcgid.ca>
2016-03-25 18:47:51 -0400

    Make update() call util fn for new and missing files

--- morenines/update.py
+++ morenines/update.py
@@ -1,15 +1,13 @@
 import os
 
-import morenines.status
-
 from morenines.index import Index
-from morenines.util import get_files
+from morenines.util import get_files, get_new_and_missing
 
 
 def update(root_path, index_file, remove_missing):
     index = Index.read(index_file)
 
-    new_files, missing_files = morenines.status.status(root_path, index_file)
+    new_files, missing_files = get_new_and_missing(root_path, index)
 
     index.add(new_files)
 
