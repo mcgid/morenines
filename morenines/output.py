aabf8c97b00b369da13237c3bd0960ecfb2a7dfd
Dan McGinnis <dan@mcgid.ca>
2016-03-26 23:06:57 -0400

    Streamline output functionsn and colours

--- morenines/output.py
+++ morenines/output.py
@@ -1,59 +1,60 @@
 import click
 import sys
 
-def info(message):
-    click.echo(message)
-
-def good(message):
-    click.secho("Warning: " + message, fg='green')
-
-def warning(message):
-    click.secho("WARNING: " + message, fg='yellow')
-
-def error(message):
-    click.secho("ERROR: " + message, fg='red')
-    sys.exit(1)
-
+GOOD_COLOUR = 'green'
+WARN_COLOUR = 'yellow'
+BAD_COLOUR = 'red'
 
 def set_output_colour(colour):
     # Print nothing except the ANSI escape sequence
     click.secho('', nl=False, fg=colour, reset=False)
+
 
 def clear_output_colour():
     # Print nothing except the reset escape sequence
     click.secho('', nl=False, reset=True)
 
 
-def print_filelist(header, filelist, colour=None):
-    set_output_colour(colour)
+def output(message, items=[], colour=None):
+    if colour:
+        set_output_colour(colour)
 
-    click.echo(header)
+    click.echo(message)
 
-    for line in sorted(filelist):
-        click.echo("  {}".format(line))
+    for item in sorted(items):
+        click.echo("  " + item)
 
     clear_output_colour()
 
 
+def warning(message, items=[]):
+    output("WARNING: " + message, items, WARN_COLOUR)
+
+
+def error(message, items=[]):
+    output("ERROR: " + message, items, BAD_COLOUR)
+    sys.exit(1)
+
+
 def print_filelists(new_files, changed_files, missing_files):
     if not any([new_files, changed_files, missing_files]):
-        good("Index is up-to-date (no changes)")
+        output("Index is up-to-date (no changes)", GOOD_COLOUR)
         return
 
     if new_files:
-        print_filelist("Added files (not in index):", new_files, 'green')
+        output("Added files (not in index):", new_files, WARN_COLOUR)
 
         # Print a blank space between sections
         if changed_files or missing_files:
             click.echo()
 
     if changed_files:
-        print_filelist("Changed files (hash differs from index):", changed_files, 'red')
+        output("Changed files (hash differs from index):", changed_files, BAD_COLOUR)
 
         # Print a blank space between sections
         if missing_files:
             click.echo()
 
     if missing_files:
-        print_filelist("Missing files:", missing_files, 'red')
+        output("Missing files:", missing_files, WARN_COLOUR)
 
