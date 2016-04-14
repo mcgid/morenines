17bf5a6a198eed631e06974e36aa5f93051486be
Dan McGinnis <dan@mcgid.ca>
2016-04-13 20:10:23 -0400

    Add test fixture data_dir and rename module_dir

--- tests/test_system.py
+++ tests/test_system.py
@@ -8,18 +8,32 @@ from morenines import application
 
 
 @pytest.fixture
-def test_dir(request):
+def module_dir(request):
     mod_file = request.module.__file__
-    test_dir , _ = os.path.splitext(mod_file)
+    module_dir , _ = os.path.splitext(mod_file)
 
-    return test_dir
+    return module_dir
 
 
 @pytest.fixture
-def example_dir(test_dir, tmpdir):
-    dest_path = tmpdir.join(os.path.basename(test_dir))
+def data_dir(request, tmpdir, module_dir):
+    test_name = request.function.__name__
 
-    data_dir = os.path.join(test_dir, "data0")
+    source_path = os.path.join(module_dir, test_name)
+
+    dest_path = tmpdir.join(test_name)
+
+    shutil.copytree(source_path, dest_path.strpath)
+
+    return dest_path.strpath
+
+
+
+@pytest.fixture
+def example_dir(module_dir, tmpdir):
+    dest_path = tmpdir.join(os.path.basename(module_dir))
+
+    data_dir = os.path.join(module_dir, "data0")
 
     shutil.copytree(data_dir, dest_path.strpath)
 
@@ -36,7 +50,7 @@ def test_init(example_dir):
     assert os.path.isdir(mn_dir) == True
 
 
-def test_create(test_dir, example_dir):
+def test_create(module_dir, example_dir):
     # Prepare the environment
     result_mn_dir = os.path.join(example_dir, ".morenines")
     os.mkdir(result_mn_dir)
@@ -54,7 +68,7 @@ def test_create(test_dir, example_dir):
         result_index = [line for line in f.readlines() if not line.startswith("date: ")]
 
 
-    premade_index_path = os.path.join(test_dir, "index-after_create")
+    premade_index_path = os.path.join(module_dir, "index-after_create")
 
     with open(premade_index_path) as f:
         premade_index = f.readlines()
