184a6c3952291f3d764555fb69187f36fb5e4268
Dan McGinnis <dan@mcgid.ca>
2016-04-14 16:09:55 -0400

    Add expected_dir fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,3 +21,11 @@ def data_dir(request, tmpdir, module_dir):
     shutil.copytree(source_path, dest_path.strpath)
 
     return dest_path.strpath
+
+@pytest.fixture
+def expected_dir(request, module_dir):
+    test_name = request.function.__name__
+
+    dir_name = test_name + '-expected'
+
+    return os.path.join(module_dir, dir_name)
--- tests/test_create.py
+++ tests/test_create.py
@@ -4,7 +4,7 @@ from click.testing import CliRunner
 from morenines import application
 
 
-def test_create(module_dir, data_dir):
+def test_create(data_dir, expected_dir):
     runner = CliRunner()
     result = runner.invoke(application.main, ['create', data_dir])
 
@@ -16,9 +16,9 @@ def test_create(module_dir, data_dir):
     with open(result_index_path) as f:
         result_index = [line for line in f.readlines() if not line.startswith("date: ")]
 
-    premade_index_path = os.path.join(module_dir, "index-after_create")
+    expected_index_path = os.path.join(expected_dir, '.morenines', 'index')
 
-    with open(premade_index_path) as f:
-        premade_index = f.readlines()
+    with open(expected_index_path) as f:
+        expected_index = f.readlines()
 
-    assert result_index == premade_index
+    assert result_index == expected_index
