5960b7aed55cd10addb8be435bbda3c990e34f88
Dan McGinnis <dan@mcgid.ca>
2016-04-14 18:15:10 -0400

    Add test for create with empty repo dir

--- tests/test_create.py
+++ tests/test_create.py
@@ -22,3 +22,23 @@ def test_create(data_dir, expected_dir):
         expected_index = f.readlines()
 
     assert result_index == expected_index
+
+
+def test_create_empty(data_dir, expected_dir):
+    runner = CliRunner()
+    result = runner.invoke(application.main, ['create', data_dir])
+
+    # Prepare for checks
+    result_index_path = os.path.join(data_dir, '.morenines', 'index')
+
+    assert os.path.isfile(result_index_path)
+
+    with open(result_index_path) as f:
+        result_index = [line for line in f.readlines() if not line.startswith("date: ")]
+
+    expected_index_path = os.path.join(expected_dir, '.morenines', 'index')
+
+    with open(expected_index_path) as f:
+        expected_index = f.readlines()
+
+    assert result_index == expected_index
