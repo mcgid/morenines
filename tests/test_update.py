e4a1c9151ce16dc4a07bf3ca6ae4242cb51368eb
Dan McGinnis <dan@mcgid.ca>
2016-04-15 12:23:14 -0400

    Add test for update command with no changes

--- tests/test_update.py
+++ tests/test_update.py
@@ -4,6 +4,29 @@ from click.testing import CliRunner
 from morenines import application
 
 
+def test_update_no_changes(data_dir, expected_dir):
+    runner = CliRunner()
+    result = runner.invoke(application.main, ['update', data_dir])
+
+    assert result.exit_code == 0
+
+    expected_output = u"Index is up-to-date (no new or missing files)\n"
+
+    assert result.output == expected_output
+
+    result_index_path = os.path.join(data_dir, '.morenines', 'index')
+
+    with open(result_index_path) as f:
+        result_index = [line for line in f.readlines() if not line.startswith("date: ")]
+
+    expected_index_path = os.path.join(expected_dir, '.morenines', 'index')
+
+    with open(expected_index_path) as f:
+        expected_index = f.readlines()
+
+    assert result_index == expected_index
+
+
 def test_update_add(data_dir, expected_dir):
     runner = CliRunner()
     result = runner.invoke(application.main, ['update', '--add-new', data_dir])
