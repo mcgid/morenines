650833361354acb58b43e81febfcb00be1490f42
Dan McGinnis <dan@mcgid.ca>
2016-04-14 20:36:53 -0400

    Add tests for status command

---
+++ tests/test_status.py
@@ -0,0 +1,42 @@
+import os
+from click.testing import CliRunner
+
+from morenines import application
+
+
+def test_status_no_changes_with_color(data_dir):
+    runner = CliRunner()
+    result = runner.invoke(application.main, ['status', data_dir])
+
+    expected_output = u"\033[32mIndex is up-to-date (no changes)\n\033[0m"
+
+    assert result.output == expected_output
+
+
+def test_status_no_changes_no_color(data_dir):
+    runner = CliRunner()
+    result = runner.invoke(application.main, ['status', '--no-color', data_dir])
+
+    expected_output = u"Index is up-to-date (no changes)\n"
+
+    assert result.output == expected_output
+
+
+def test_status_new_file_with_color(data_dir):
+    runner = CliRunner()
+    result = runner.invoke(application.main, ['status', data_dir])
+
+    # Result output is unicode, so we need a unicode string literal
+    expected_output = u"\033[33mNew files (not in index):\n  2012/new_file.txt\n\033[0m"
+
+    assert result.output == expected_output
+
+
+def test_status_new_file_no_color(data_dir):
+    runner = CliRunner()
+    result = runner.invoke(application.main, ['status', '--no-color', data_dir])
+
+    # Result output is unicode, so we need a unicode string literal
+    expected_output = u"New files (not in index):\n  2012/new_file.txt\n"
+
+    assert result.output == expected_output
