78ed8d52b83bb6f1a2b37e067441600be8cdd4f1
Dan McGinnis <dan@mcgid.ca>
2016-04-15 10:11:01 -0400

    Add test for create with existing index

--- tests/test_create.py
+++ tests/test_create.py
@@ -42,3 +42,14 @@ def test_create_empty(data_dir, expected_dir):
         expected_index = f.readlines()
 
     assert result_index == expected_index
+
+
+def test_create_with_existing_index(data_dir):
+    runner = CliRunner()
+    result = runner.invoke(application.main, ['create', data_dir])
+
+    expected_output_prefix = u"ERROR: Index file already exists:"
+
+    starts_with_expected = result.output.startswith(expected_output_prefix)
+
+    assert starts_with_expected == True
